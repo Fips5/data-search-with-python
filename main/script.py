84d4708e98238c6f455306167171d383da7110be
Fips <david00badea@gmail.com>
2023-04-13 02:04:13 +0300

    aded a demo for c, r, s

--- main/script.py
+++ main/script.py
@@ -5,12 +5,23 @@ import os
 from openpyxl import load_workbook
 
 while True:
-    dir_path = "./files"        #       CHANGE THE PATH FOR THE FOLDER YOU HAVE, PUT THE SPECIFIC PATH
+    dir_path = "./files(example folder)"  # CHANGE THE PATH FOR THE FOLDER YOU HAVE, PUT THE SPECIFIC PATH
 
     xlsx_files = [f for f in os.listdir(dir_path) if f.endswith(".xlsx")]
 
-    row_num = int(input("Enter the row number: "))
-    col_letter = input("Enter the column letter: ")
+    choice = input("Do you want to check a row (r), column (c), or specific cell (s)? ")
+    if choice.lower() == "r":
+        row_num = int(input("Enter the row number: "))
+        col_letter = None
+    elif choice.lower() == "c":
+        col_letter = input("Enter the column letter: ")
+        row_num = None
+    elif choice.lower() == "s":
+        col_letter = input("Enter the column letter: ")
+        row_num = int(input("Enter the row number: "))
+    else:
+        print("Invalid choice. Please enter r, c, or s.")
+        continue
 
     cell_values = {}
 
@@ -18,20 +29,37 @@ while True:
         filepath = os.path.join(dir_path, filename)
         workbook = load_workbook(filepath)
         sheet = workbook.active
-        cell = sheet["{}{}".format(col_letter, row_num)]
-        cell_values[filename] = cell.value
+        if row_num is not None:
+            values = []
+            for cell in sheet.iter_rows(min_row=row_num, max_row=row_num):
+                for c in cell:
+                    values.append(c.value)
+            cell_values[filename] = values
+        elif col_letter is not None:
+            values = []
+            col_index = ord(col_letter) - 96
+            for cell in sheet.iter_cols(min_col=col_index, max_col=col_index):
+                for c in cell:
+                    values.append(c.value)
+            cell_values[filename] = values
+        else:
+            cell = sheet["{}{}".format(col_letter, row_num)]
+            cell_values[filename] = [cell.value]
 
     all_values_same = all(value == list(cell_values.values())[0] for value in cell_values.values())
 
     if all_values_same:
-        print("All files have the same value: {}".format(list(cell_values.values())[0]))
+        print("All files have the same value: {}".format(list(cell_values.values())[0][0]))
     else:
+        for filename, values in cell_values.items():
+            if len(set(values)) == 1:
+                print("{}: All values are the same: {}".format(filename, values[0]))
+            else:
+                for idx, val in enumerate(values):
+                    if val != values[0]:
+                        print("{}: Cell {}{} has a different value: {}".format(filename, col_letter, row_num+idx, val))
 
-        for filename, value in cell_values.items():
-            if value != list(cell_values.values())[0]:
-                print("{}: {}".format(filename, value))
-        
-        normal_value = list(cell_values.values())[0]
+        normal_value = list(cell_values.values())[0][0]
         print("Normal value from other files: {}".format(normal_value))
 
     user_choice = input("Do you want to exit? y/n: ")
@@ -43,3 +71,4 @@ while True:
         print("Invalid choice. Please enter y or n.")
 
 print("Program has ended.")
+
